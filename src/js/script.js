fdede7fe6dfeee49e56c319b545410153913dfcd
Tomomi Tanabe <benata1121@outlook.jp>
2023-07-01 20:31:31 +0900

    ピクパ終了

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,21 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       // $('.js-drawer-menu').fadeOut();
       $('.js-drawer-menu').removeClass('is-open');
       $(this).removeClass('is-open');
+      $('body').css('overflow-y','auto'); 
     } else {
       // $('.js-drawer-menu').fadeIn();
       $('.js-drawer-menu').addClass('is-open');
       $(this).addClass('is-open');
+      $('body').css('overflow-y', 'hidden');
+
     }
+  });
+
+  // ウィンドウがリサイズされたとき
+ 
+  $(window).resize(function(){ // ウィンドウがリサイズされたら
+    $('.js-drawer-menu').removeClass('is-open');
+    $('.js-hamburger').removeClass('is-open');
   });
 
   // スクロールすると背景色変更
@@ -48,33 +58,31 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 });
 
 
-  // swiper MV
-  var swiper1 = new Swiper(".js-mv-swiper", {
-    loop: true,
-    effect: "fade", // フェード切り替え
-    // 自動再生
-    autoplay: {
-      delay: 4000, // 4秒後に次のスライドへ
-      disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
-    },
-    speed: 2000, // 2秒かけてフェード
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  // // swiper MV
+  // var swiper1 = new Swiper(".js-mv-swiper", {
+  //   loop: true,
+  //   effect: "fade", // フェード切り替え
+  //   // 自動再生
+  //   autoplay: {
+  //     delay: 4000, // 4秒後に次のスライドへ
+  //     disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+  //   },
+  //   speed: 2000, // 2秒かけてフェード
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
 
-  // swiper Works
-  var swiper2 = new Swiper(".js-works-swiper", {
-    pagination: {
-      el: ".js-works-pagination",
-    },
-    loop: true,
-    clickable: true,
-    autoplay: {
-          delay: 4000, // 4秒後に次のスライドへ
-          disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
-        },
-  });
-
-  
+  // // swiper Works
+  // var swiper2 = new Swiper(".js-works-swiper", {
+  //   pagination: {
+  //     el: ".js-works-pagination",
+  //     clickable: true,
+  //   },
+  //   loop: true,
+  //   autoplay: {
+  //         delay: 4000, // 4秒後に次のスライドへ
+  //         disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+  //       },
+  // });
