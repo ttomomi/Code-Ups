8670b5dbad163e7bbc5d2e80f3d992784f45fdbd
Tomomi Tanabe <benata1121@outlook.jp>
2023-07-01 20:32:32 +0900

    サーバーアップ前最終チェック終了

--- src/js/script.js
+++ src/js/script.js
@@ -58,31 +58,31 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 });
 
 
-  // // swiper MV
-  // var swiper1 = new Swiper(".js-mv-swiper", {
-  //   loop: true,
-  //   effect: "fade", // フェード切り替え
-  //   // 自動再生
-  //   autoplay: {
-  //     delay: 4000, // 4秒後に次のスライドへ
-  //     disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
-  //   },
-  //   speed: 2000, // 2秒かけてフェード
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-  // });
+  // swiper MV
+  var swiper1 = new Swiper(".js-mv-swiper", {
+    loop: true,
+    effect: "fade", // フェード切り替え
+    // 自動再生
+    autoplay: {
+      delay: 4000, // 4秒後に次のスライドへ
+      disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+    },
+    speed: 2000, // 2秒かけてフェード
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-  // // swiper Works
-  // var swiper2 = new Swiper(".js-works-swiper", {
-  //   pagination: {
-  //     el: ".js-works-pagination",
-  //     clickable: true,
-  //   },
-  //   loop: true,
-  //   autoplay: {
-  //         delay: 4000, // 4秒後に次のスライドへ
-  //         disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
-  //       },
-  // });
+  // swiper Works
+  var swiper2 = new Swiper(".js-works-swiper", {
+    pagination: {
+      el: ".js-works-pagination",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+          delay: 4000, // 4秒後に次のスライドへ
+          disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+        },
+  });
